844ac3247f4dc37d97e6420047c42ca748f944cd
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-21 22:25:12 -0400

    fixing get single route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -26,11 +26,9 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag }]
+      include: [Category, { model: Tag, as: 'tags' }]
     });
 
     if (!productData) {
